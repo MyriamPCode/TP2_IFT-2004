861a987b01fa62348aa44ca7262296a4aed42f01
Meryele <mariecharlotte.coutaud@gmail.com>
2023-10-31 10:41:00 -0400

    Create TP2.sql

---
+++ TP2.sql
@@ -0,0 +1,23 @@
+drop table TP2_UTILISATEUR cascade constraints;
+drop table TP2_ENTREPRISE cascade constraints;
+drop table TP2_PROJET cascade constraints;
+drop table TP2_UTILISATEUR_PROJET cascade constraints;
+drop table TP2_PROFIL_ACCESSIBILITE cascade constraints;
+drop table TP2_PROFIL_ACCESSIBILITE_IMAGE cascade constraints;
+drop table TP2_PROFIL_ACCESSIBILITE_IMAGE_COORDONNEE cascade constraints;
+drop table TP2_PROFIL_ACCESSIBILITE_PLAN cascade constraints;
+drop table TP2_PROFIL_ACCESSIBILITE_PLAN_COORDONNEE cascade constraints;
+drop table TP2_SONDAGE cascade constraints;
+drop table TP2_TYPE_QUESTION cascade constraints;
+drop table TP2_QUESTION cascade constraints;
+drop table TP2_CHOIX_REPONSE cascade constraints;
+drop table TP2_REPONSE_UTILISATEUR cascade constraints;
+
+create table TP2_UTILISATEUR (
+    NO_UTILISATEUR number(6) not null,
+    COURRIEL_UTI varchar2(40) not null,
+    MOT_DE_PASSE_UTI varchar2(40) not null,
+    PRENOM_UTI varchar2(40) not null,
+    NOM_UTI varchar2(40) not null,
+    TYPE_UTI varchar2(14) not null,
+    constraint PK_UTILISATEUR primary key(NO_UTILISATEUR));
