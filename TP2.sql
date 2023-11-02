df8ba161e0a4a2f82874eca239dddaad815c7d6a
Meryele <mariecharlotte.coutaud@gmail.com>
2023-11-02 11:22:19 -0400

    Add new table
    Ajout des table TYPE_QUESTION,  QUESTION, CHOIX_REPONSE ET UTILISATEUR

--- TP2.sql
+++ TP2.sql
@@ -31,5 +31,41 @@ create table TP2_ENTREPRISE (
     CODE_POSTAL_ENT char(7) not null,
     VILLE_ENT varchar2(40) not null,
     COURRIEL_ENT varchar(2) not null,
-    NO_ENTREPRISE_DIRIGEANTE number(6) not null
+    NO_ENTREPRISE_DIRIGEANTE number(6) not null,
     constraint PK_ENTREPRISE primary key(NO_ENTREPRISE));
+    
+create table TP2_TYPE_QUESTION (
+    CODE_TYPE_QUESTION char(4) not null,
+    DESC_TYPE_QUE varchar2(40) not null,
+    constraint PK_TYPE_QUESTION primary key(CODE_TYPE_QUESTION));
+    
+create table TP2_QUESTION (
+    ID_QUESTION number(6) not null,
+    ORDRE_QUESTION number(3) default 1 not null,
+    CODE_TYPE_QUESTION char(4) not null,
+    TEXTE_QUE varchar2(100) not null,
+    NO_SONDAGE number(6) not null,
+    constraint PK_QUESTION primary key(ID_QUESTION),
+    constraint FK_CODE_TYPE_QUESTION foreign key(CODE_TYPE_QUESTION)
+        references TP2_TYPE_QUESTION(CODE_TYPE_QUESTION)/*,
+    constraint FK_NO_SONDAGE foreign key(NO_SONDAGE)
+        references TP2_SONDAGE(NO_SONDAGE)*/);
+        
+create table TP2_CHOIX_REPONSE (
+    ID_CHOIX_REPONSE number(6) not null,
+    ORDRE_REPONSE number(3) not null,
+    TEXTE_CHO varchar2(100) not null,
+    ID_QUESTION number(6) not null,
+    constraint PK_CHOIX_REPONSE primary key(ID_CHOIX_REPONSE),
+    constraint FK_ID_QUESTION foreign key(ID_QUESTION)
+        references TP2_QUESTION(ID_QUESTION));
+        
+create table TP2_REPONSE_UTILISATEUR (
+    NO_UTILISATEUR number(6) not null,
+    ID_CHOIX_REPONSE number(6) not null,
+    TEXTE_REP varchar2(1000) not null,
+    constraint PK_REPONSE_UTILISATEUR primary key(NO_UTILISATEUR, ID_CHOIX_REPONSE),
+    constraint FK_NO_UTILISATEUR foreign key(NO_UTILISATEUR)
+        references TP2_UTILISATEUR(NO_UTILISATEUR),
+    constraint FK_ID_CHOIX_REPONSE foreign key(ID_CHOIX_REPONSE)
+        references TP2_CHOIX_REPONSE(ID_CHOIX_REPONSE));
